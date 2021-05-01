9c6e4bf33e02a57cc228325086c51418a7fafac9
mustafa.akcay <akcay.mustafa@metu.edu.tr>
2021-05-01 02:51:15 +0000

    added javadoc

--- server/src/main/java/com/example/server/repository/PlayerRepository.java
+++ server/src/main/java/com/example/server/repository/PlayerRepository.java
@@ -1,16 +1,16 @@
 package com.example.server.repository;
 
 import com.example.server.model.Player;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
+/**
+ * This class is repository for Player entity.
+ * Since username and email are unique fields, functions regarding finding a player using
+ * these fields are added to repository.
+ * @author Nureddin Alperen Ustun & Mustafa Ali Akcay
+ *
+ */
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
     Player findPlayerByUsername(String username);
