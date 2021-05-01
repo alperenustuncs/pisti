226bb6a4d62410d357c90bde7f53b940c00945f1
mustafa.akcay <akcay.mustafa@metu.edu.tr>
2021-05-01 02:54:13 +0000

    added javadoc

--- server/src/main/java/com/example/server/service/ScoreServiceImpl.java
+++ server/src/main/java/com/example/server/service/ScoreServiceImpl.java
@@ -11,39 +11,69 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
+/**
+ * This class is the implementation of the ScoreService interface. This class is a wrapper class
+ * for crud operations of the Score entity.
+ * @author Nureddin Alperen Ustun & Mustafa Ali Akcay
+ */
 @Service
 public class ScoreServiceImpl implements ScoreService{
 
-    private final long DAY_IN_MS = 1000 * 60 * 60 * 24;
+    private final long DAY_IN_MS = 1000 * 60 * 60 * 24;//Total mili seconds in one day
     @Autowired
     private ScoreRepository scoreRepository;
     @Autowired
     private PlayerRepository playerRepository;
 
+    /**
+     *
+     * @param score score to be added
+     * @return added score
+     */
     @Override
     public Score addScore(Score score) {
         return scoreRepository.save(score);
     }
 
+    /**
+     *
+     * @return all the scores
+     */
     @Override
     public List<Score> getAllScores() {
         return scoreRepository.findAll();
     }
 
+    /**
+     *
+     * @return weekly high scores by descending order
+     */
     public List<Score> getWeeklyHighScores(){
 
         return scoreRepository.findAllByScoreTimeBetweenOrderByScoreDesc(new Date(System.currentTimeMillis() - (7 * DAY_IN_MS)),new Date());
     }
 
+    /**
+     *
+     * @return monthly high scores by descending order
+     */
     public List<Score> getMonthlyHighScores(){
         return scoreRepository.findAllByScoreTimeBetweenOrderByScoreDesc(new Date(System.currentTimeMillis() - (30 * DAY_IN_MS)),new Date());
     }
 
+    /**
+     *
+     * @return all time high scores by descending order
+     */
     public List<Score> getAllTimeHighScores(){
         return scoreRepository.findAllByOrderByScoreDesc();
     }
 
+    /**
+     * Updates given score
+     * @param score score to be updated
+     * @return updated score
+     */
     @Override
     public Score updateScore(Score score) {
         Score scoreToUpdate = scoreRepository.getOne(score.getId());
@@ -53,6 +83,10 @@ public class ScoreServiceImpl implements ScoreService{
         return scoreToUpdate;
     }
 
+    /**
+     * Deletes scores with given ids
+     * @param idList score id list
+     */
     @Override
     public void deleteScore(List<Integer> idList) {
         for (int id : idList) {
@@ -60,18 +94,12 @@ public class ScoreServiceImpl implements ScoreService{
         }
     }
 
-//    username unique olmaliymis
-//    @Override
-//    public List<Score> findScore(JSONObject jsonScore) {
-////        int id = jsonScore.getInt("id");
-////        return scoreRepository.getOne(id);
-//        String username = jsonScore.getString("username");
-//        return scoreRepository.findScoreByUsername(username);
-//        //return scoreRepository.findAll(username);
-//    }
 
-
-
+    /**
+     * From json with id, it returns score object.
+     * @param jsonScore json which has id in it
+     * @return desired score object
+     */
     @Override
     public Score findScore(JSONObject jsonScore) {
         int id = jsonScore.getInt("id");
